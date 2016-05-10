078930a76312b2ca064ba9e5922c3653e0a228e5
Thr1ve <fict1on.buhler@gmail.com>
2016-05-09 20:04:56 -0400

    hook CodeStep up to reducers

--- app/scenes/CodeStep/index.js
+++ app/scenes/CodeStep/index.js
@@ -10,19 +10,8 @@ import ProgressBar from './scenes/ProgressBar';
 import Notes from './scenes/Notes';
 import Code from './scenes/Code';
 
-import code from '../../code.js';
-
-// const border = (color) => ({ border: `5px solid ${color}` });
-const steps = [
-  { highlighted: [1], note: 'hello there' },
-  { highlighted: [2], note: 'how are you' },
-  { highlighted: [3] },
-  { highlighted: [5] },
-  { highlighted: [7, 8, 9] },
-  { highlighted: [10] },
-  { highlighted: [12] },
-  { highlighted: [14, 15, 16, 17, 18, 19, 20, 21, 22] },
-];
+import lesson from '../../lessons/test1';
+const { code, steps } = lesson;
 
 const Layout = React.createClass({
   componentDidMount() {
