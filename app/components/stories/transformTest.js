dfa345ee687e017d5e48a4b8309a2f937394ac1f
Thr1ve <fict1on.buhler@gmail.com>
2016-05-11 21:15:51 -0400

    cleanup and improve transitionTest

--- app/components/stories/transformTest.js
+++ app/components/stories/transformTest.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
 const boxStyle = {
-  height: '100px',
-  width: '100px',
+  height: '150px',
+  width: '150px',
   background: 'lightblue',
   fontSize: '8px',
   transformStyle: 'preserve-3d'
@@ -19,35 +19,72 @@ const Slider = ({ valueLink, label = '', ...props }) =>
         valueLink.set(e.target.value);
       }}
     />
+    {valueLink.value}
   </label>;
 
-const TransformBox = ({ x, y, z, perspective }) =>
+const TransformBox = ({
+  perspective,
+  tx, ty, tz,
+  rx, ry, rz,
+  scx, scy, scz,
+  skx, sky
+}) =>
   <div style={{
     ...boxStyle,
     transform: `
-      translateX(${x}vw)
-      translateY(${y}vh)
-      translateZ(${z}vh)
+      translateX(${tx}vw)
+      translateY(${ty}vh)
+      translateZ(${tz}vh)
+      rotateX(${rx}deg)
+      rotateY(${ry}deg)
+      rotateZ(${rz}deg)
+      scaleX(${scx})
+      scaleY(${scy})
+      scaleZ(${scz})
+      skewX(${skx}deg)
+      skewY(${sky}deg)
     `
-  }}
->
-    translateX: {x}
+  }}>
+    translateX: {tx}
     <br />
-    translateY: {y}
+    translateY: {ty}
     <br />
-    translateZ: {z}
+    translateZ: {tz}
     <br />
     perspective: {perspective}
+    <br />
+    rotateX: {rx}
+    <br />
+    rotateY: {ry}
+    <br />
+    rotateY: {rz}
+    <br />
+    ScaleX: {scx}
+    <br />
+    ScaleY: {scy}
+    <br />
+    ScaleY: {scz}
+    <br />
+    SkewX: {skx}
+    <br />
+    SkewY: {sky}
   </div>;
+
+const defaultValues = {
+  tx: 0, ty: 0, tz: 0,
+  rx: 0, ry: 0, rz: 0,
+  scx: 1, scy: 1, scz: 1,
+  skx: 0, sky: 0,
+  perspective: 1000,
+};
 
 const Controller = React.createClass({
   getInitialState() {
-    return {
-      tx: 1,
-      ty: 1,
-      tz: 1,
-      perspective: 1,
-    };
+    return defaultValues;
+  },
+
+  reset() {
+    this.setState(defaultValues);
   },
 
   render() {
@@ -55,17 +92,34 @@ const Controller = React.createClass({
       <div style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <div style={{ height: '80vh', width: '100vw', perspective: `${this.state.perspective}px`, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
           <TransformBox
-            x={this.state.tx}
-            y={this.state.ty}
-            z={this.state.tz}
+            tx={this.state.tx}
+            ty={this.state.ty}
+            tz={this.state.tz}
             perspective={this.state.perspective}
+            rx={this.state.rx}
+            ry={this.state.ry}
+            rz={this.state.rz}
+            scx={this.state.scx}
+            scy={this.state.scy}
+            scz={this.state.scz}
+            skx={this.state.skx}
+            sky={this.state.sky}
           />
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', height: '20vh' }}>
-          <Slider label="X" valueLink={linkState(this, 'tx')} max={100} min={0} step={0.5} />
-          <Slider label="Y" valueLink={linkState(this, 'ty')} max={100} min={0} step={0.5} />
-          <Slider label="Z" valueLink={linkState(this, 'tz')} max={500} min={0} step={10} />
+          <button onClick={this.reset}>Reset</button>
+          <Slider label="TransformX" valueLink={linkState(this, 'tx')} max={100} min={-100} step={1} />
+          <Slider label="TransformY" valueLink={linkState(this, 'ty')} max={100} min={-100} step={1} />
+          <Slider label="TransformZ" valueLink={linkState(this, 'tz')} max={100} min={-100} step={1} />
           <Slider label="Perspective" valueLink={linkState(this, 'perspective')} max={1000} min={0} step={10} />
+          <Slider label="RotateX" valueLink={linkState(this, 'rx')} max={180} min={-180} step={1} />
+          <Slider label="RotateY" valueLink={linkState(this, 'ry')} max={180} min={-180} step={1} />
+          <Slider label="RotateZ" valueLink={linkState(this, 'rz')} max={180} min={-180} step={1} />
+          <Slider label="ScaleX" valueLink={linkState(this, 'scx')} max={100} min={0} step={0.5} />
+          <Slider label="ScaleY" valueLink={linkState(this, 'scy')} max={100} min={0} step={0.5} />
+          <Slider label="ScaleZ" valueLink={linkState(this, 'scz')} max={100} min={0} step={0.5} />
+          <Slider label="SkewX" valueLink={linkState(this, 'skx')} max={180} min={-180} step={1} />
+          <Slider label="SkewY" valueLink={linkState(this, 'sky')} max={180} min={-180} step={1} />
         </div>
       </div>
     );
