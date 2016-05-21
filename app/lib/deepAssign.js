34368ec76d3468a0b804231a38393c4402109064
Thr1ve <fict1on.buhler@gmail.com>
2016-05-21 17:15:43 -0400

    use Object.assign in deepAssign

--- app/lib/deepAssign.js
+++ app/lib/deepAssign.js
@@ -6,7 +6,7 @@ export function deepAssign(oldObj, newObj) {
     if (typeof newObj[key] === 'object' && !!oldObj[key]) {
       updated[key] = deepAssign(oldObj[key], newObj[key]);
     } else {
-      updated[key] = newObj[key];
+      updated[key] = Object.assign({}, newObj[key]);
     }
   }
 
@@ -16,24 +16,24 @@ export function deepAssign(oldObj, newObj) {
   };
 }
 
-// let obj1 = {
-//   foo: 'bar',
-//   nested: {
-//     baz: 'buz',
-//     beep: 'boop'
-//   }
-// };
-//
-// let obj2 = {
-//   hi: 'there',
-//   nested: {
-//     baz: 'NOTBUZ'
-//   },
-//   nested2: {
-//     otherBaz2: 'otherBuz2'
-//   }
-// };
-//
-// let copy = deepAssign(obj1, obj2);
+let obj1 = {
+  foo: 'bar',
+  nested: {
+    baz: 'buz',
+    beep: 'boop'
+  }
+};
 
-// console.log(copy);
+let obj2 = {
+  hi: 'there',
+  nested: {
+    baz: 'NOTBUZ'
+  },
+  nested2: {
+    otherBaz2: 'otherBuz2'
+  }
+};
+
+let copy = deepAssign(obj1, obj2);
+
+console.log(copy);
