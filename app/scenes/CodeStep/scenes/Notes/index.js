607a44e99e5f1cbfc86f5aaa756e200bae1a4898
Thr1ve <fict1on.buhler@gmail.com>
2016-05-22 18:01:09 -0400

    remove buttons from notes

--- app/scenes/CodeStep/scenes/Notes/index.js
+++ app/scenes/CodeStep/scenes/Notes/index.js
@@ -1,36 +1,17 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { notes } from './styles.css';
+import styles from './styles.css';
 
-import { nextStep, previousStep } from '../../services';
+const Notes = ({ note }) => (
+  <div className={styles.notes}>
+    {note}
+  </div>
+);
 
-const Notes = React.createClass({
-  propTypes: {
-    note: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
-  },
-
-  next() {
-    this.props.dispatch(nextStep());
-  },
-
-  previous() {
-    this.props.dispatch(previousStep());
-  },
-
-  render() {
-    const { note } = this.props;
-    return (
-      <div className={notes}>
-        {note}
-        <div style={{ float: 'right' }}>
-          <button onClick={this.previous}>Previous</button>
-          <button onClick={this.next}>Next</button>
-        </div>
-      </div>
-    );
-  }
-});
+Notes.propTypes = {
+  note: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
+};
 
 const mapStateToProps = (state) => ({ note: state.note.current });
 
