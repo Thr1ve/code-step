a8a8ea107425c21f142f68dd379191e3ca7a07c3
Thr1ve <fict1on.buhler@gmail.com>
2016-05-23 16:11:17 -0400

    update storybook webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,5 +5,8 @@ module.exports = {
     loaders: [
       {test: /\.css$/, loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'},
     ]
-  }
-}
+  },
+  resolve: {
+    modulesDirectories: ['app', 'node_modules'],
+  },
+};
