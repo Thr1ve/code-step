3f1257445a074f6be9e8cc42d2e9cd04c976181c
Thr1ve <fict1on.buhler@gmail.com>
2016-05-23 20:47:24 -0400

    add namespace for transformTester state

--- app/config/rootReducer.js
+++ app/config/rootReducer.js
@@ -2,9 +2,11 @@
 import { combineReducers } from 'redux';
 
 import codeStep from '../scenes/CodeStep/services/reducer';
+import transformTester from '../scenes/TransformTester/services/reducer';
 
 const rootReducer = combineReducers({
-  codeStep
+  codeStep,
+  transformTester
 });
 
 export default rootReducer;
