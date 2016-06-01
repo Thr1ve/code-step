a4057045750026f224cc83f0aed0aa111fcbdbeb
Thr1ve <fict1on.buhler@gmail.com>
2016-06-01 16:19:38 -0400

    move CodeSelect into Menu

--- app/scenes/CodeStep/scenes/Menu/index.js
+++ app/scenes/CodeStep/scenes/Menu/index.js
@@ -1,21 +1,17 @@
+// This is only a here because I might expand CodeSelect into a full menu later
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import styles from './styles.css';
 
-import { setCurrentLesson } from '../../services';
+import CodeSelect from '../CodeSelect';
 
 const Menu = ({ visible, dispatch }) => (
   <div
     className={styles.menu}
     style={{ display: `${visible ? 'flex' : 'none'}` }}
   >
-    It's a Menu
-    <div
-      onClick={() => dispatch(setCurrentLesson('__NONE__'))}
-    >
-      select new lesson
-    </div>
+    <CodeSelect />
   </div>
 );
 
