a6d9683cc3e06890f53693adab9b7e6144aea02c
Thr1ve <fict1on.buhler@gmail.com>
2016-06-05 19:47:03 -0400

    update fizzBuzz example

--- lessons/FizzBuzz.js
+++ lessons/FizzBuzz.js
@@ -26,105 +26,125 @@ export const steps = [
   }, {
     // zoom: 0.9,
     highlighted: [1, 13],
-    note: `work in progress`,
+    note: '[explain fizzBuzz problem]',
     scopes() {
     }
 // first iteration
   }, {
     highlighted: [2, 12],
-    note: `work in progress`,
+    note: 'To start, we\'ll simply loop through numbers 1 to 100 using a simple for-loop',
     scopes() {
       this.fizzBuzz = { i: 1 };
     }
   }, {
-    highlighted: [3, 4],
-    note: `work in progress`,
+    highlighted: [3],
+    note: 'Next, we\'ll check if i divides evenly into both 5 and 3. Since the value of currently 1, this conditional will not pass, and line 4 will not execute.',
     scopes() {
     }
   }, {
-    highlighted: [5, 6],
-    note: `work in progress`,
+    highlighted: [5],
+    note: 'Next, we\'ll check if i divides evenly into just 3. Again, this conditional will not pass, and line 6 will not execute.',
     scopes() {
     }
   }, {
-    highlighted: [7, 8],
-    note: `work in progress`,
+    highlighted: [7],
+    note: 'Next, we\'ll check if i divides evenly into just 5. Again, this conditional will not pass, and line 8 will not execute.',
     scopes() {
     }
   }, {
-    highlighted: [9, 10, 11],
-    note: `work in progress`,
+    highlighted: [9, 11],
+    note: 'Since none of the conditionals in the above if statements passed, the code in our else block will now be executed.',
+    scopes() {
+    }
+  }, {
+    highlighted: [10],
+    note: 'The current value of i (1) gets logged to the console!',
     scopes() {
     }
   }, {
 // second iteration
     highlighted: [2],
-    note: `work in progress`,
+    note: '[work in progress]',
     scopes() {
       this.fizzBuzz.i = 2;
     }
   }, {
-    highlighted: [3, 4],
-    note: `work in progress`,
+    highlighted: [3],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [5, 6],
-    note: `work in progress`,
+    highlighted: [5],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [7, 8],
-    note: `work in progress`,
+    highlighted: [7],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [9, 10, 11],
-    note: `work in progress`,
+    highlighted: [9, 11],
+    note: '[work in progress]',
+    scopes() {
+    }
+  }, {
+    highlighted: [10],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
 // third iteration
     highlighted: [2],
-    note: `work in progress`,
+    note: '[work in progress]',
     scopes() {
       this.fizzBuzz.i = 3;
     }
   }, {
-    highlighted: [3, 4],
-    note: `work in progress`,
+    highlighted: [3],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [5, 6],
-    note: `work in progress`,
+    highlighted: [5],
+    note: '[work in progress]',
+    scopes() {
+    }
+  }, {
+    highlighted: [6],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
 // fourth iteration
     highlighted: [2],
-    note: `work in progress`,
+    note: '[work in progress]',
     scopes() {
       this.fizzBuzz.i = 4;
     }
   }, {
-    highlighted: [3, 4],
-    note: `work in progress`,
+    highlighted: [3],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [5, 6],
-    note: `work in progress`,
+    highlighted: [5],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [7, 8],
-    note: `work in progress`,
+    highlighted: [7],
+    note: '[work in progress]',
     scopes() {
     }
   }, {
-    highlighted: [9, 10, 11],
-    note: `work in progress`,
+    highlighted: [9, 11],
+    note: '[work in progress]',
+    scopes() {
+    }
+  }, {
+    highlighted: [10],
+    note: '[work in progress]',
     scopes() {
     }
   }
